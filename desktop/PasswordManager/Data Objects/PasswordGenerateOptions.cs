0620f8eba3e35ffae1d049324e18e87d05d38ad8
eminozkan <info@eminozkan.net>
2023-06-05 10:19:54 +0300

    Finished Desktop App

---
+++ desktop/PasswordManager/Data Objects/PasswordGenerateOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Data_Objects
+{
+    internal class PasswordGenerateOptions
+    {
+
+        public PasswordGenerateOptions() { 
+            hasSpecialCharacters = false;
+            hasNumericCharacters = false;
+            hasLowerCaseCharacters = true;
+            hasUpperCaseCharacters = false;
+        }
+
+        public int length;
+
+        public bool hasSpecialCharacters;
+
+        public bool hasNumericCharacters;
+
+        public bool hasUpperCaseCharacters;
+
+        public bool hasLowerCaseCharacters;
+    }
+}
--- desktop/PasswordManager/Services/HttpRequestHandlerService.cs
+++ desktop/PasswordManager/Services/HttpRequestHandlerService.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,5 +54,63 @@ namespace PasswordManager.Services
         
         }
 
+        public static string POST(object data, string url, string path,string token)
+        {
+            try
+            {
+                var myContent = JsonConvert.SerializeObject(data);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(url);
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    var result = client.PostAsync(path, byteContent).Result;
+                    return result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Server is not responding!";
+            }
+        }
+
+        public static string GET(string url, string path, string token)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(url);
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    var result = client.GetAsync(path).Result;
+                    return result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Server is not responding!";
+            }
+        }
+
+        internal static string DELETE(string url, string path, string bearerToken)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(url);
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+                    var result = client.DeleteAsync(path).Result;
+                    return result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Server is not responding!";
+            }
+        }
     }
 }
